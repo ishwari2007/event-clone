c5e043fef25c3c7b6870ecc7693b1f7fcde683cb
ishwari2007 <ishwarigovalkar@gmail.com>
2026-03-11 15:06:08 +0530

    Tested GET APIs for events

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,35 @@
-console.log("hello");
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+// connect database
+connectDB();
+
+
+// routes
+app.use("/api/events", require("./routes/eventRoutes"));
+
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Event Management API Running");
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
